43a966b88c23a15b4f3a126f5270173a21115dfb
jdelak <delakdev@gmail.com>
2019-07-31 23:23:36 +0200

    modif etape 12

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ $multiplicateur = document.getElementById("multiplicateur");
 $autoclicker = document.getElementById("autoclicker");
 $score = 0;
 $compteur = 1;
+$nbClick = 1;
 $nextBuy = (50 * $compteur);
-$acNextbuy = 200;
+$acNextbuy = 200 * $nbClick;
 $img = document.getElementById("img");
 
 
 $button.onclick = myFunction;
 $multiplicateur.onclick = incrementer;
 $multiplicateur.innerHTML = "Multiplicateur x "+$compteur+" (prochain achat : "+$nextBuy+")";
-$autoclicker.innerHTML = "DPS : "+$compteur+" ("+$acNextbuy+")";
+$autoclicker.innerHTML = "DPS : "+$compteur+" (prochain achat : "+$acNextbuy+")";
 $autoclicker.onclick = autoclicker;
 
 /*if($score >= (50 * $compteur)){
@@ -40,15 +41,18 @@ function incrementer(event){
 		$multiplicateur.innerHTML = "Multiplicateur x "+$compteur;
 		$nextBuy = (50 * $compteur);
 		$multiplicateur.innerHTML = "Multiplicateur x "+$compteur+" (prochain achat : "+$nextBuy+")";
-		$autoclicker.innerHTML = "DPS : "+$compteur+" ("+$acNextbuy+")";
+		$autoclicker.innerHTML = "DPS : "+($compteur * $nbClick)+" (prochain achat : "+$acNextbuy+")";
 	}
 	
 }
 
 function autoclicker(event){
-	if($score >= 200 ){
-		$score = $score - 200;
+	if($score >= (200 * $nbClick) ){
+		$score = $score - (200 * $nbClick);
+		$nbClick++;
 		$affichage.innerHTML = $score;
+		$acNextbuy = (200 * $nbClick);
+		$autoclicker.innerHTML = "DPS : "+$compteur * $nbClick+" (prochain achat : "+$acNextbuy+")";
 		setInterval(myFunction, 1000);
 	}
 }
